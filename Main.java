a7ef087662e0b6466a588b829790b52ae0414098
adrianF21500 <flo21500@uvg.edu.gt>
2022-02-12 10:49:51 -0600

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+class Main{
+
+	public static void main(String[] args){
+
+		PosfixCalc calc = new PosfixCalc();
+		
+		ArrayList<String> lineas = new ArrayList<String>();
+
+	}
+}
---
+++ PosfixCalc.java
@@ -0,0 +1,13 @@
+class PosfixCalc implements IPosfixCalc{
+
+	/***
+	 * This method is used to evaluate a postfix expression
+	 * @param expresion this is a single expression like: 1 2 + 4 * 3 +
+	 * @return the evaluation of the expression
+	 */
+	@Override
+	public int Evaluate(String expresion){
+		StackArrayList<Integer> stack = new StackArrayList<Integer>();
+		return 0;
+	}
+}
